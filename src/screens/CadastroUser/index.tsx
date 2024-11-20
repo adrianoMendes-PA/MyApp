78eb26958709856a24f03640ee6ac39292f99823
Adriano Mendes <adriano.alyson@gmail.com>
2024-11-20 17:36:14 -0300

    style da tela de cadastro

--- src/screens/CadastroUser/index.tsx
+++ src/screens/CadastroUser/index.tsx
@@ -1,5 +1,13 @@
 import React from 'react';
+import {Container} from './style';
+import {Text} from 'react-native';
 
 export default () => {
-  return <></>;
+  return (
+    <>
+      <Container>
+        <Text>Tela de Cadastro</Text>
+      </Container>
+    </>
+  );
 };
