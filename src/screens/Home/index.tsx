ab58e9c0467215045c281adbfd812562dd14d97a
Adriano Mendes <adriano.alyson@gmail.com>
2024-11-26 19:47:31 -0300

    componente status bar

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import {Text} from 'react-native';
+import StatusBar from '../../components/StatusBar';
 
 export default () => {
   return (
     <>
+      <StatusBar />
       <Text>TELA HOME</Text>
     </>
   );
